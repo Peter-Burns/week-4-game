ee89ba1ca38da38c837e5621b6dcff3862b6abaf
Peter Burns <peterburnshicks@gmail.com>
2017-11-05 19:50:55 -0500

    added functions to write to the DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,28 @@
 var crystalValues = {
-	diamond: 0,
+	diamond: 5,
 	ruby: 0,
 	sapphire: 0,
 	emerald:0
 };
-var targetScore;
+var targetScore=19+Math.floor(100*Math.random());
 var runningTotal=0;
-console.log(1);
+var wins=0;
+var losses=0;
 $('.crystalPic').on('click',function(){
-console.log(this.value);
+runningTotal+=crystalValues[this.value];
+console.log(runningTotal);
+console.log(targetScore);
 });
+function updateRunningTotal(){
+	$('#runningTotal').text(runningTotal);
+}
+function updateTargetScore(){
+	$('#targetScore').text(targetScore);
+}
+function updateWinLoss(result){
+	$('#result').text('You '+result+'!');
+	$('wins').text('Wins: '+wins);
+	$('losses').text('Losses: '+losses);
+}
+updateRunningTotal();
+updateTargetScore();
