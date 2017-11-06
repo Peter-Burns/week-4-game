342a66f579626800517c9f2ffd48a8b54817e452
Peter Burns <peterburnshicks@gmail.com>
2017-11-05 19:39:32 -0500

    wrapped img elements in buttons and added on click handler in js

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var crystalValues = {
+	diamond: 0,
+	ruby: 0,
+	sapphire: 0,
+	emerald:0
+};
+var targetScore;
+var runningTotal=0;
+console.log(1);
+$('.crystalPic').on('click',function(){
+console.log(this.value);
+});
